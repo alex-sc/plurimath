c6352592db79cfd4056edd996d9a1bd4185c687c
alex <alex-sc@users.noreply.github.com>
2025-03-21 11:18:03 +0200

    Parslet: remove the min_length optimization (426/900 seconds)

--- lib/plurimath/asciimath/parslet_extensions.rb
+++ lib/plurimath/asciimath/parslet_extensions.rb
@@ -10,11 +10,6 @@ module Parslet
 end
 
 class Parslet::Atoms::Base
-  def min_length
-    puts "min_length must be implemented in #{self.class}"
-    exit!
-  end
-
   def lookahead(source)
     puts "lookahead must be implemented in #{self.class}"
     exit!
@@ -22,37 +17,14 @@ class Parslet::Atoms::Base
 end
 
 class Parslet::Atoms::Alternative < Parslet::Atoms::Base
-
-  def initialize(*alternatives)
-    super()
-    if alternatives.size == 3329
-      #puts caller
-      #exit!
-      puts alternatives.size
-      alternatives.each_index do |i|
-        puts "#{i+1}. #{alternatives[i].class} #{alternatives[i].inspect}"
-      end
-    end
-
-    @alternatives = alternatives
-  end
-
-  def |(parslet)
-    self.class.new(*@alternatives + [parslet])
-  end
-
-  def min_length
-    @min_len if @min_len
-    @min_len = @alternatives.map(&:min_length).min
-  end
-
   def lookahead(source)
+    # We need to stop the recursive lookahead at some point, otherwise it might go too deep
     true
   end
 
   def try(source, context, consume_all)
     alternatives.map { |a|
-      next if source.chars_left < a.min_length || not a.lookahead(source)
+      next unless a.lookahead(source)
       #puts "Trying out of #{alternatives.length}, min=#{a.min_length}"
 
       success, _ = result = a.apply(source, context, consume_all)
@@ -65,48 +37,24 @@ class Parslet::Atoms::Alternative < Parslet::Atoms::Base
 end
 
 class Parslet::Atoms::Sequence < Parslet::Atoms::Base
-  def initialize(*parslets)
-    super()
-
-    @parslets = parslets
-    @min_len = parslets.map(&:min_length).sum
-  end
-
-  def min_length
-    @min_len
-  end
-
   def lookahead(source)
     parslets[0].lookahead(source)
   end
 end
 
 class Parslet::Atoms::Str < Parslet::Atoms::Base
-  def min_length
-    @len
-  end
-
   def lookahead(source)
     source.lookahead(@pat)
   end
 end
 
 class Parslet::Atoms::Named < Parslet::Atoms::Base
-  def min_length
-    @parslet.min_length
-  end
-
   def lookahead(source)
     parslet.lookahead(source)
   end
 end
 
 class Parslet::Atoms::Repetition < Parslet::Atoms::Base
-  def min_length
-    return 0 if @min == 0
-    @parslet.min_length * @min
-  end
-
   def lookahead(source)
     true if @min == 0
     parslet.lookahead(source)
@@ -114,26 +62,22 @@ class Parslet::Atoms::Repetition < Parslet::Atoms::Base
 end
 
 class Parslet::Atoms::Re < Parslet::Atoms::Base
-  def min_length
-    # TODO: is zero possible?
-    # TODO?: we could try to detect if an expression requires > 1 symbol, but it's too much effort
-    1
-  end
-
   def lookahead(source)
     source.lookahead(@re)
   end
 end
 
 class Parslet::Atoms::Dynamic < Parslet::Atoms::Base
-  def min_length
-    0
+  def lookahead(source)
+    # TODO?
+    true
   end
 end
 
 class Parslet::Atoms::Capture < Parslet::Atoms::Base
-  def min_length
-    0
+  def lookahead(source)
+    # TODO?
+    true
   end
 end
 
